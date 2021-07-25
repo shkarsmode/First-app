36a42767810b902aae53f8aceef5cd336d1a1e55
shkarsmode <zshkarrr@gmail.com>
2021-07-25 14:49:50 +0300

    commit1

---
+++ src/App.js
@@ -0,0 +1,14 @@
+// import React, {Component} from "react";
+import "./App.css";
+import Header from "./components/Header";
+
+const App = () => {  
+  return (
+    <div className="">
+      <Header />
+    </div>
+  );
+};
+
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+const Header = () =>{
+	return(
+		<div>
+			<ul>
+				<li>Menu</li>
+				<li>Home</li>
+				<li>Tape1</li>
+				<li>Tape2</li>
+			</ul>
+		</div>
+	);
+}
+
+export default Header;
