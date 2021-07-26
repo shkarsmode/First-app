f8e377e1b22893bba5939ac5141f7009f0f88ef0
shkarsmode <zshkarrr@gmail.com>
2021-07-26 22:59:53 +0300

    more comp

---
+++ src/components/News.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const News = () =>{
+	return(
+	<div className="content">
+	<div className="center">
+		<div className="news">
+			news
+		</div>
+		<div className="news">
+			Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda iste nobis neque quisquam quasi ab, rem consequatur architecto modi deserunt cupiditate laborum ducimus nesciunt laboriosam quas soluta, ea, in officiis! Maiores iusto, nihil aliquid saepe debitis nemo aut repellat sapiente distinctio dolorum? Nihil odio possimus praesentium placeat ullam quaerat quis modi vel rem, delectus veritatis. Qui mollitia doloribus reiciendis perspiciatis, veniam dignissimos nesciunt fugit suscipit. Explicabo repellat amet sequi, laboriosam similique incidunt quos. Rem, vel. Reiciendis laudantium natus quasi, rem repudiandae suscipit ratione odit eveniet dolor nemo error voluptatum. Magni id alias cum deserunt quae quis. Doloremque suscipit tenetur dolores!
+		</div>
+	</div>
+	<div className="right">
+		<div className="block">
+				block
+		</div>
+	</div>
+</div>
+);
+}
+
+export default News;
