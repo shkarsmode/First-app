e63e1492a31cee6c5f7358bae32a66a72ef6133c
shkarsmode <zshkarrr@gmail.com>
2021-07-29 21:10:02 +0300

    browserrouter

--- src/components/Content/Content.js
+++ src/components/Content/Content.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
 import Dialogs from '../Dialogs/Dialogs';
 import MyPage from '../MyPage/MyPage';
 import Nav from '../Nav/Nav';
@@ -7,16 +8,17 @@ import "./Content.css";
 
 const Content = () => {
 	return(
+		<BrowserRouter>
 		<div className="wrap-center">
 			<div className="wrap-content">
 				<Nav />
-				
-				<Dialogs />
-				{/* <MyPage /> */}
-				{/* <News /> */}
+				<Route component={MyPage}/>
+				<Route component={Dialogs}/>
+				<Route component={News}/>
 
 			</div>
 		</div>
+		</BrowserRouter>
 	);
 }
 
