8193c59609028de7db3dcbc82620911d4fce6ece
shkarsmode <zshkarrr@gmail.com>
2021-08-10 11:18:24 +0300

    rerender

--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import state from './redux/state';
-import {addPost, addMessage} from './redux/state';
+import {rerenderEntireTree} from './render';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App state={state} addPost={addPost} addMessage={addMessage}/>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+rerenderEntireTree(state);
 
 
-reportWebVitals();
+
+
+
 
