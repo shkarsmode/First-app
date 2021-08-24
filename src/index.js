9855772ee70606b8ca807508ed1a1916ff82296a
shkarsmode <zshkarrr@gmail.com>
2021-08-24 14:16:38 +0300

    create context

--- src/index.js
+++ src/index.js
@@ -8,10 +8,10 @@ import { BrowserRouter } from 'react-router-dom';
 let rerenderEntireTree = (state)=> {
   ReactDOM.render(
 		<BrowserRouter>
-    <React.StrictMode>
-      <App state={state} 
-           dispatch={store.dispatch.bind(store)}/>
-    </React.StrictMode>
+      <React.StrictMode>
+        <App state={state} 
+            dispatch={store.dispatch.bind(store)}/>
+      </React.StrictMode>
 		</BrowserRouter>,
     document.getElementById('root')
   );
