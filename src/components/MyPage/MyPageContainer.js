6fe9a59df4501e06815750aefc25aff06fbce732
shkarsmode <zshkarrr@gmail.com>
2021-08-24 14:33:46 +0300

    create storecontext(mypagecontainer.js)

--- src/components/MyPage/MyPageContainer.js
+++ src/components/MyPage/MyPageContainer.js
@@ -3,24 +3,31 @@ import { addPostCre, updatePostCre } from '../../redux/post-reducer';
 import MyPage from './MyPage';
 import Post from './Post/Post';
 import Friend from './Friend/Friend';
+import StoreContext from '../../storeContext';
 
+const MyPageContainer = () =>{
+	return <StoreContext>{
+			(store)=>{
+				let state = store.getState();
+				let dispatch = store.dispatch;
+				let posts = state.postsData.map(post => (<Post id={post.id} title={post.title} />));
+				let friends = state.friendsData.map(friend => (<Friend name={friend.name} url={friend.url}/>));
 
-const MyPageContainer = (props) =>{
-	let posts = props.state.postsData.map(post => (<Post id={post.id} title={post.title} />));
-  let friends = props.state.friendsData.map(friend => (<Friend name={friend.name} url={friend.url}/>));
-
-	let createPost = (el, input)=>{
-		if(el.code == "Enter" && input != '')
-			props.dispatch(addPostCre());
-	}
-	let updatePost = (text)=> props.dispatch(updatePostCre(text));
-	
-	return <MyPage 
-		updateNewPostText = {text => updatePost(text)}
-		createPost = {el => createPost(el)} 
-		posts = {posts}
-		friends = {friends}
-		newPostText = {props.state.newPostText}/>
+				let createPost = (el, input)=>{
+					if(el.code == "Enter" && input != '')
+						dispatch(addPostCre());
+				}
+				let updatePost = (text)=> dispatch(updatePostCre(text));
+				
+				return <MyPage 
+					updateNewPostText = {updatePost}
+					createPost = {createPost} 
+					posts = {posts}
+					friends = {friends}
+					newPostText = {state.newPostText}
+					/>
+			}	
+		}</StoreContext>
 }
 
 export default MyPageContainer;
--- src/index.js
+++ src/index.js
@@ -4,13 +4,15 @@ import './index.css';
 import App from './App';
 import store from './redux/redux-state';
 import { BrowserRouter } from 'react-router-dom';
+import StoreContext from './storeContext';
 
 let rerenderEntireTree = (state)=> {
   ReactDOM.render(
 		<BrowserRouter>
       <React.StrictMode>
-        <App state={state} 
-            dispatch={store.dispatch.bind(store)}/>
+        <StoreContext.Provider value={store}>
+        <App />
+        </StoreContext.Provider>
       </React.StrictMode>
 		</BrowserRouter>,
     document.getElementById('root')
