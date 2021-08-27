5c02f7790b5103ad49a15c87f5b42516e6a347bf
shkarsmode <zshkarrr@gmail.com>
2021-08-28 00:42:35 +0300

    top users.js

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,8 +1,28 @@
 import React from 'react';
+import d from './Users.module.css';
 
 const Users = () => {
 	return (
-		<div>123</div>
+		<div className={d.wrap}>
+			<div className={d.first}>
+				<div className={d.block}>
+					<div className={d.top}><span>Friend requests <span className={d.req}>346</span></span><span className={d.show}>Show all </span></div>
+					<div className={d.bottom}>
+						<img src="" alt="" />
+						<div>
+							<span>Alyona Sumskaya</span>
+							<span>Alyona Sumskaya and 8 others mutual friends</span>
+							<div><button>Accept request</button><button>Keep as follower</button></div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className={d.second}>
+				<div className={d.active}>All chats</div>
+				<div>Unread</div>
+				<div>Starred messages</div>
+			</div>
+		</div>
 	)
 }
 
