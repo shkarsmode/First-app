2e73b04bbbfa89b829cb4e6547149e9645465501
shkarsmode <zshkarrr@gmail.com>
2021-09-03 16:48:25 +0300

    refact message.js

--- src/components/Dialogs/Messages/MessagesContainer.js
+++ src/components/Dialogs/Messages/MessagesContainer.js
@@ -4,38 +4,6 @@ import Messages from './Messages';
 import Message from './Message';
 import {connect} from 'react-redux';
 
-// const MessagesContainer = () =>{
-// 	return <StoreContext.Consumer>
-// 		{
-// 		(store)=>{
-// 			let dispatch = store.dispatch;
-// 			let dialogsPage = store.getState().dialogsPage;
-// 			let messages = dialogsPage.messegesData.map(mess => <Message name={mess.name} mess={mess.mess} url={mess.url}/>);
-// 			let sendMessage = (input)=> {
-// 				if(input != '')
-// 					sendMessageHelp(input);
-// 			}
-
-// 			let sendMessage2 = (el, input)=> {
-// 				if(el.code == "Enter" && input != '')
-// 					sendMessageHelp(input);
-// 			}
-	
-// 			let updateMessage = input => dispatch(updateMessageCre(input));
-// 			let sendMessageHelp = input => dispatch(addMessageCre(input));
-	
-// 			return (
-// 				<Messages 
-// 					messages = {messages}
-// 					updateMessage ={updateMessage}
-// 					sendMessage = {sendMessage}
-// 					sendMessage2 = {sendMessage2}
-// 					newMessageText = {dialogsPage.newMessageText}
-// 					onloadOnresize = {dialogsPage.onloadOnresize}/>)
-// 		}}
-// 		</StoreContext.Consumer>
-// }
-
 let mapStateToProps = state => {
 	let messages = state.dialogsPage.messegesData.map(mess => <Message name={mess.name} mess={mess.mess} url={mess.url}/>);
 	return{
